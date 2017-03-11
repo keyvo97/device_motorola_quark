285fdea37a13fef8b9ad16b5e478f1ca7d39f6c2
keyvo97 <romullonerd2015@gmail.com>
2017-03-10 21:18:34 -0300

    Aosp Tree

---
+++ aosp_quark.mk
@@ -0,0 +1,16 @@
+$(call inherit-product, device/motorola/quark/full_quark.mk)
+
+# Inherit some common AOSP stuff.
+$(call inherit-product, vendor/aosp/common.mk)
+
+# Overlay
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+
+PRODUCT_RELEASE_NAME := Moto MAXX
+PRODUCT_NAME := aosp_quark
+
+PRODUCT_GMS_CLIENTID_BASE := android-motorola
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_BUILD_FLAVOR=quark-$(TARGET_BUILD_VARIANT)
+
